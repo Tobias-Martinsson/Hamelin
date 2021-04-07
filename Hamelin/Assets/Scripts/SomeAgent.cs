01d3f53a5421a1d8706510e52fa7ff5d3b2df7d1
Tobias-Martinsson <59065729+Tobias-Martinsson@users.noreply.github.com>
2021-04-07 14:41:53 +0200

    v2 av catching net

--- Hamelin/Assets/Scripts/SomeAgent.cs
+++ Hamelin/Assets/Scripts/SomeAgent.cs
@@ -31,18 +31,7 @@ public class SomeAgent : MonoBehaviour
 
     private void Update()
     {
-        point1 = transform.position + collider.center + Vector3.up * (collider.height / 2 - collider.radius);
-        point2 = transform.position + collider.center + Vector3.down * (collider.height / 2 - collider.radius);
         StateMachine.RunUpdate();
-
-
-        if(Physics.CapsuleCast(point1, point2, collider.radius * 0.95f, Vector3.forward, out hit, forwardCheckDistance))
-        {
-            Debug.Log(hit.transform.tag);
-            if (hit.transform.tag == "CatchingNet")
-                GetComponent<Renderer>().enabled = false;
-                Destroy(this);
-        }
     }
 
     void OnTriggerEnter(Collider other)
