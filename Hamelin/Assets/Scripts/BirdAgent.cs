168cb6e6f6f2c2eadb8ca9a871f2f85fa7b32e69
Tobias-Martinsson <59065729+Tobias-Martinsson@users.noreply.github.com>
2021-04-08 18:51:34 +0200

    bird bomb try 2

---
+++ Hamelin/Assets/Scripts/BirdAgent.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BirdAgent : SomeAgent
+{
+    public float timeLeft = 5.0f;
+    public float originalTime;
+    public GameObject myPrefab;
+    private StateMachine StateMachine;
+
+    private void Start()
+    {
+        originalTime = timeLeft;
+    }
+
+    private void Awake()
+    {
+        collider = GetComponent<CapsuleCollider>();
+        NavAgent = GetComponent<NavMeshAgent>();
+        StateMachine = new StateMachine(this, States);
+    }
+
+
+
+    private void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            Instantiate(myPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            timeLeft = originalTime;
+        }
+
+        StateMachine.RunUpdate();
+    }
+
+}
