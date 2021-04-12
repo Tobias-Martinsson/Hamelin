ca61bb87bc42b73b8c77dd8ccce0f45e5fc608d7
PinkPenguinKing <henrik_ruden@hotmail.com>
2021-04-12 14:40:43 +0200

    Grapling
    TEST

--- Hamelin/Assets/Scripts/PlayerController3D.cs
+++ Hamelin/Assets/Scripts/PlayerController3D.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController3D : MonoBehaviour
@@ -53,11 +55,29 @@ public class PlayerController3D : MonoBehaviour
     private StateMachine StateMachine;
     public CapsuleCollider collider;
 
+
+    //Grapple Test
+
+    [SerializeField] float pullSpeed = 0.5f;
+    [SerializeField] float stopDistance = 4f;
+    [SerializeField] GameObject hookPrefab;
+    [SerializeField] Transform shootTransform;
+
+    Hook hook;
+    bool pulling;
+    Rigidbody rigid;
+    //
+
     private void Awake() => collider = GetComponent<CapsuleCollider>();
 
     // Start is called before the first frame update
     private void Start()
     {
+        //grappling hook test
+        rigid = GetComponent<Rigidbody>();
+        pulling = false;
+        //
+
         StateMachine = new StateMachine(this, States);
         jumpPower = Vector3.up * jumpPowerVariable;
         startMaxSpeed = maxSpeed;
@@ -179,6 +199,33 @@ public class PlayerController3D : MonoBehaviour
         }
 
         StateMachine.RunUpdate();
+
+        //Grappling hook test
+        if (hook == null && Input.GetMouseButtonDown(0))
+        {
+            StopAllCoroutines();
+            pulling = false;
+            hook = Instantiate(hookPrefab, shootTransform.position, Quaternion.identity).GetComponent<Hook>();
+            hook.Initialize(this, shootTransform);
+            StartCoroutine(DestroyHookAfterLifetime());
+        }
+        else if (hook != null && Input.GetMouseButtonDown(1))
+        {
+            DestroyHook();
+        }
+
+        if (!pulling || hook == null) return;
+
+        if (Vector3.Distance(transform.position, hook.transform.position) <= stopDistance)
+        {
+            DestroyHook();
+        }
+        else
+        {
+            rigid.AddForce((hook.transform.position - transform.position).normalized * pullSpeed, ForceMode.VelocityChange);
+            //Accelerate(velocity);
+        }
+        //
     }
 
     private bool WaitTime(float seconds)
@@ -274,4 +321,27 @@ public class PlayerController3D : MonoBehaviour
             velocity += normalForce;
         }
     }
+
+    public void StartPull()
+    {
+        pulling = true;
+    }
+
+    private void DestroyHook()
+    {
+        if (hook == null) return;
+
+        pulling = false;
+        Destroy(hook.gameObject);
+        hook = null;
+    }
+
+    private IEnumerator DestroyHookAfterLifetime()
+    {
+        yield return new WaitForSeconds(8f);
+
+        DestroyHook();
+    }
+
+
 }
