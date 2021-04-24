77c3d0849914e21184250473e5f40466f40f9dc9
TeaTiger <cfasch@gmail.com>
2021-04-24 10:12:35 +0200

    Added UI
    La till lite kod i BugNetController för att skapa en UI Counter som håller koll på hur många saker man har fångat.

---
+++ Hamelin/Assets/s_EnemyScoreCounter.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class s_EnemyScoreCounter : MonoBehaviour
+{
+
+    private Text scoreCounter;
+
+    public SphereCollider collider;
+    public BugNetController bugNet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       scoreCounter = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreCounter.text = bugNet.Score.ToString();
+
+    }
+
+    private void AddScore()
+    {
+    }
+}
