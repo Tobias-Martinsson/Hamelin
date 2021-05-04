489563a58852a5297c0a0b39dc88e9f5180db6bd
ZuchiniMuumuu <frejazuchinimuumuu@gmail.com>
2021-05-04 20:39:26 +0200

    Delayed repeat created, added to rats with squeak clips

---
+++ Hamelin/Assets/Scripts/AudioScripts/DelayedRepeat.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class DelayedRepeat : MonoBehaviour
+{
+    private AudioSource source;
+
+    private float minDelay = 3.0f;
+    private float maxDelay = 15.0f;
+
+    public AudioClip[] clips;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            int clipIndex = Random.Range(1, clips.Length);
+            AudioClip clip = clips[clipIndex];
+            source.clip = clip;
+
+            float delay = Random.Range(minDelay, maxDelay);
+            source.PlayDelayed(delay);
+
+            clips[clipIndex] = clips[0];
+            clips[0] = clip;
+        }
+    }
+}
