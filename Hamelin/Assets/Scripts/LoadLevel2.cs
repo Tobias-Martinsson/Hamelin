942748285d9aebd35154c7790739599012de88a1
Tobias-Martinsson <59065729+Tobias-Martinsson@users.noreply.github.com>
2021-05-05 15:56:01 +0200

    Update LoadLevel2.cs

--- Hamelin/Assets/Scripts/LoadLevel2.cs
+++ Hamelin/Assets/Scripts/LoadLevel2.cs
@@ -9,16 +9,6 @@ public class LoadLevel2 : MonoBehaviour
     public BoxCollider col;
     void Awake() => col = GetComponent<BoxCollider>();
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
