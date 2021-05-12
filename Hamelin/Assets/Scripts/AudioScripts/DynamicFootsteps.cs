2edb69827f57970dad28d6c04b26e4fbb23c9190
ZuchiniMuumuu <frejazuchinimuumuu@gmail.com>
2021-05-12 18:41:15 +0200

    Started on footstep sounds

--- Hamelin/Assets/Scripts/AudioScripts/DynamicFootsteps.cs
+++ Hamelin/Assets/Scripts/AudioScripts/DynamicFootsteps.cs
@@ -7,6 +7,8 @@ public class DynamicFootsteps : MonoBehaviour
 {
     private AudioSource source;
 
+    private string colliderType;
+
     public AudioClip[] defaultSteps;
     public AudioClip[] woodSteps;
     public AudioClip[] grassSteps;
@@ -17,13 +19,51 @@ public class DynamicFootsteps : MonoBehaviour
         source = GetComponent<AudioSource>();
     }
 
-    void Update()
+    void OnCollisionEnter(Collision other)
     {
-        
+        SurfaceColliderType act = other.gameObject.GetComponent<Collider>().gameObject.GetComponent<SurfaceColliderType>();
+
+        if (act)
+        {
+            colliderType = act.GetTerrainType();
+        }
     }
 
     public void PlayDynamicFootstep()
     {
-
+        if (!source.isPlaying)
+        {
+            switch (colliderType)
+            {
+                case "Wood":
+                    int clipIndex1 = Random.Range(1, woodSteps.Length);
+                    AudioClip clip1 = woodSteps[clipIndex1];
+                    source.PlayOneShot(clip1);
+                    woodSteps[clipIndex1] = woodSteps[0];
+                    woodSteps[0] = clip1;
+                    break;
+                case "Grass":
+                    int clipIndex2 = Random.Range(1, grassSteps.Length);
+                    AudioClip clip2 = grassSteps[clipIndex2];
+                    source.PlayOneShot(clip2);
+                    grassSteps[clipIndex2] = grassSteps[0];
+                    grassSteps[0] = clip2;
+                    break;
+                case "Car":
+                    int clipIndex3 = Random.Range(1, carSteps.Length);
+                    AudioClip clip3 = carSteps[clipIndex3];
+                    source.PlayOneShot(clip3);
+                    carSteps[clipIndex3] = carSteps[0];
+                    carSteps[0] = clip3;
+                    break;
+                default:
+                    int clipIndex4 = Random.Range(1, defaultSteps.Length);
+                    AudioClip clip4 = defaultSteps[clipIndex4];
+                    source.PlayOneShot(clip4);
+                    defaultSteps[clipIndex4] = defaultSteps[0];
+                    defaultSteps[0] = clip4;
+                    break;
+            }
+        }       
     }
 }
