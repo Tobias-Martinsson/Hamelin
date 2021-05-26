099284df425728a8ed31db58d58ab5b5097f12ea
MyKarlen <Myy.karlen@gmail.com>
2021-05-26 11:33:45 +0200

    Minor changes

--- Hamelin/Assets/Scripts/UI scripts/PausMeny.cs
+++ Hamelin/Assets/Scripts/UI scripts/PausMeny.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PausMeny : MonoBehaviour
+public class PauseMeny : MonoBehaviour
 {
     public GameObject pauseMeny;
     // Start is called before the first frame update
