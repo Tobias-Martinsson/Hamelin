3879c0f1959034be0b0cf82026db5ff3dc5626c0
Tobias-Martinsson <59065729+Tobias-Martinsson@users.noreply.github.com>
2021-06-01 14:47:58 +0200

    LADDERFIX

--- Hamelin/Assets/Scripts/UI scripts/PopUp.cs
+++ Hamelin/Assets/Scripts/UI scripts/PopUp.cs
@@ -7,55 +7,47 @@ public class PopUp : MonoBehaviour
 {
     public GameObject popup;
     public Text ptext;
-    public bool knowladder;
     
     // Start is called before the first frame update
     void Start()
     {
         DeActivatePopup();
-        knowladder = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-         if(knowladder){
-             if(Input.GetKeyDown(KeyCode.E)){
-                DeActivatePopup();
-                knowladder = false;
-             }
-         }
+         
     }
 
-     private void OnTriggerEnter(Collider collider)
+     private void OnTriggerStay(Collider collider)
      {
-         if(this.gameObject.tag == "LadderTrigger"){
-             
+        
+         if(collider.gameObject.tag == "Player")
+        {
             ptext = ptext.GetComponent<Text>();
-             ptext.text = "Press E to climb";
-             ActivatePopup();
-             knowladder = true;
-
-
-         }
+            ptext.text = "Press E to climb";
+            ActivatePopup();
+        }
      }
 
-     private void ActivatePopup(){
-         if(!popup.activeInHierarchy){
-             popup.SetActive(true);
-         } else{
-             DeActivatePopup();
-         }
-         
-     }
+    private void OnTriggerExit(Collider other)
+    {
+        DeActivatePopup();
+    }
 
-     private void DeActivatePopup(){
-         if(popup.activeInHierarchy){
-             popup.SetActive(false);
-         } 
-         
-     }
+    private void ActivatePopup()
+    {
 
+        popup.SetActive(true);
+    }
 
+    private void DeActivatePopup()
+    {
+        if (popup.activeInHierarchy)
+        {
+            popup.SetActive(false);
+        }
 
+    }
 }
